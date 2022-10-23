4d360cf6ac21cdf46ffc5410ac34729da45da63b
luziferus <luziferus@pm.me>
2022-10-23 17:17:35 +0200

    feat: introduce argparse

--- file.py
+++ file.py
@@ -1,29 +1,3 @@
-import re
-import getopt
-import sys
-
-file_name = 'lookup_public_pgp.py'
-
-
-def read_input(argv):
-    try:
-        opts, args = getopt.getopt(argv, "e:", ["email="])
-        if len(opts) == 0:
-            print(f'usage: {file_name} -e <email>')
-            sys.exit(2)
-
-    except getopt.GetoptError:
-        print(f'{file_name} -e <email>')
-
-    for opt, arg in opts:
-        if opt in ('-e', '--email'):
-            return arg
-
-
-def format_email_to_file_name(_email: str):
-    return re.sub('[^A-Za-z0-9_]+', '', _email)
-
-
 def save_public_key(_data: bytes, _name: str):
     with open(_name, "w") as file:
         file.write(_data.decode("utf-8"))
---
+++ pgp_lookup.py
@@ -0,0 +1,50 @@
+import sys
+import urllib.request
+import argparse
+
+from lookup_resources import load_resources, Resources
+from qr import create_qr_from_string
+from file import save_public_key
+from urllib.error import HTTPError
+
+
+def search_for_pub_key(_url: str):
+    with urllib.request.urlopen(_url) as response:
+        return response.read()
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--email", help="searches for pgp keys for given email")
+    parser.add_argument("-qr", "--qr_code", help="specifies the path for generated qr code")
+    parser.add_argument("-f", "--file", help="specifies the path for downloaded pgp key")
+    args = parser.parse_args()
+
+    resources: Resources = load_resources()
+    found_pgp = None
+
+    for remote in resources.remote:
+        if args.email:
+            url = remote.path.format(email=args.email)
+            try:
+                print(f"trying for Email: {args.email} and remoter provider {remote.name}...")
+                found_pgp = search_for_pub_key(url)
+            except HTTPError:
+                continue
+        else:
+            sys.stdout.write('Provide at least email param')
+            sys.exit(2)
+
+    if found_pgp is None:
+        sys.stderr.write(f'No public PGP keys were found!')
+        sys.exit(2)
+
+    if args.qr_code:
+        create_qr_from_string(args.qr_code, url)
+        print(f'QR: {args.qr_code}')
+    if args.file:
+        save_public_key(found_pgp, args.file)
+        print(f'file path: {args.file}')
+
+    sys.stdout.write(found_pgp.decode("utf-8"))
+
